2b4d32e823225ac84c3a0bd116fd4f013eef7189
miladnemati <mnem0001@student.monash.edu>
2023-03-04 10:51:30 +1100

    initial ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
 }
