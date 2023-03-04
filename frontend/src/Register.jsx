eff46373189e6c6d110c4c70ef3a8f9858c50b26
Alex Wilson <47777467+Adub648@users.noreply.github.com>
2023-03-04 13:00:23 +1100

    UI for register

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -12,15 +12,16 @@ export const Register = (props) => {
 
     return (
         <div className="auth-form-container">
-            <h2>Register</h2>
+            <img class="logo" src={require('./img/logo.png')} alt="Frienderlies logo" />
+            <h2 class="title">Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
+            <label class="subtitle" htmlFor="name">Full name</label>
             <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
+            <label class="subtitle" htmlFor="email">email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
+            <label class="subtitle" htmlFor="password">password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Log In</button>
+            <button type="submit" class="login">Continue</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
